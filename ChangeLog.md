da76950e05581b34f5cba5dda3cf5473143fd311
Daniel Firth <dan.firth@homotopic.tech>
2020-11-10 19:02:35 +0000

    Add reinterpretUnder, reinterpretUnder2 and reinterpret2Under

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,9 @@
 # Changelog for polysemy-extra
 
+## v0.1.1.0
+
+* Add `reinterpretUnder`, `reinterpretUnder2` and `reinterpret2Under`.
+
 ## v0.1.0.0
 
 * Add several `Input`, `Output` and `KVStore` functions that I use..
--- src/Polysemy/Extra.hs
+++ src/Polysemy/Extra.hs
@@ -2,17 +2,20 @@
 {-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE GADTs          #-}
 {-# LANGUAGE LambdaCase     #-}
+{-# LANGUAGE RankNTypes     #-}
 {-# LANGUAGE PolyKinds      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators  #-}
 module Polysemy.Extra where
 
+import Control.Arrow
 import Data.Map as Map
 import Polysemy
 import Polysemy.KVStore
 import Polysemy.Input
 import Polysemy.Output
+import Polysemy.Membership
 
 -- | Run a KVStore in terms of another KVStore by way of pure key and value
 -- transformations.
@@ -50,7 +53,7 @@ runOutputMapAsKVStore :: Members '[ KVStore k v ] r
 runOutputMapAsKVStore = interpret \case
   Output xs -> mapM_ (uncurry writeKV) (Map.toList xs)
 
--- | Map an Output forwards 
+-- | Map an Output forwards
 mapOutput :: Members '[ Output o' ] r
           => (o -> o')
           -> Sem (Output o ': r) a
@@ -83,3 +86,40 @@ contramapInputSem :: forall i i' r a.
                   -> Sem r a
 contramapInputSem f = interpret \case
   Input -> f =<< input @i'
+
+-- | Reinterpret the second effect in the stack into a single effect.
+reinterpretUnder :: forall e1 e2 e3 r a.
+                    (forall m x. Sem (e2 ': m) x -> Sem (e3 ': m) x)
+                 -> Sem (e1 ': e2 ': r) a
+                 -> Sem (e1 ': e3 ': r) a
+reinterpretUnder f = raise2Under @e1 @e1 @e2
+                 >>> subsumeUsing @e1 (There Here)
+                 >>> f
+                 >>> raise2Under @e3 @e3 @e1
+                 >>> subsumeUsing @e3 (There Here)
+
+-- | Reinterpret the third effect in the stack into a single effect.
+reinterpretUnder2 :: forall e1 e2 e3 e4 r a.
+                     (forall m x. Sem (e3 ': m) x -> Sem (e4 ': m) x)
+                  -> Sem (e1 ': e2 ': e3 ': r) a
+                  -> Sem (e1 ': e2 ': e4 ': r) a
+reinterpretUnder2 f = raise3Under @e1 @e1 @e2 @e3
+                  >>> subsumeUsing @e1 (There $ There Here)
+                  >>> raise3Under @e2 @e2 @e3 @e1
+                  >>> subsumeUsing @e2 (There $ There Here)
+                  >>> f
+                  >>> raise3Under @e4 @e4 @e1 @e2
+                  >>> subsumeUsing @e4 (There $ There Here)
+
+-- | Reinterpret the second effect in the stack in terms of two effects.
+reinterpret2Under :: forall e1 e2 e3 e4 r a.
+                     (forall m x. Sem (e2 ': m) x -> Sem (e3 ': e4 ': m) x)
+                  -> Sem (e1 ': e2 ': r) a
+                  -> Sem (e1 ': e3 ': e4 ': r) a
+reinterpret2Under f = raise2Under @e1 @e1 @e2
+                  >>> subsumeUsing @e1 (There Here)
+                  >>> f
+                  >>> raise3Under @e3 @e3 @e4 @e1
+                  >>> subsumeUsing @e3 (There $ There Here)
+                  >>> raise3Under @e4 @e4 @e1 @e3
+                  >>> subsumeUsing @e4 (There $ There Here)
